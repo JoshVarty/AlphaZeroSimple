e10a048ea3afccda02b1398ae8e904ee04ea323b
Josh Varty <joshvarty@gmail.com>
2020-08-30 21:55:55 -0400

    Fixing #2

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 args = {
     'batch_size': 64,
     'numIters': 500,                                # Total number of training iterations
+    'num_simulations': 100,                         # Total number of MCTS simulations to run when deciding on a move to play
     'numEps': 100,                                  # Number of full games (episodes) to run during each iteration
     'numItersForTrainExamplesHistory': 20,
     'epochs': 2,                                    # Number of epochs of training per iteration
